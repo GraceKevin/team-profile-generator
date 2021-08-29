ad31991500d6251f97c8aabdc2e0b443021b42df
Kevin Grace <Kevin@kevins-air.home>
2021-08-29 12:43:59 -0400

    update questions, dir html

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const inquirer = require ('inquirer');
 const fs = require ('fs');
+const { inherits } = require('util');
 
-let questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the employee?'
-    },
-    {
-        type: 'input',
-        name: 'ID',
-        message: 'What is the ID of the employee?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the email of the employee?'
-    },
-    {
-        type: 'list',
-        name: 'role',
-        message: 'What is the role of the employee?',
-        choices: ['Intern', 'Manager', 'Engineer']
-    }
-]
+let employeeInfo = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the role of the employee?',
+            choices: ['Intern', 'Engineer', 'Manager']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'ID',
+            message: 'What is the ID of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the employee?'
+        },
+    ])
+}
+
+const writeFile = data => {
+    fs.writeFile ('./dis/index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    })
+}
+
+init(); 
